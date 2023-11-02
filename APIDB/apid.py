10b53cd63b6dbaeddd005472efcba501996750d7
Rizky Maulid Ardiansyh <128461559+arndns@users.noreply.github.com>
2023-11-02 10:16:27 +0700

    Update apid.py
    part 2

--- APIDB/apid.py
+++ APIDB/apid.py
@@ -6,116 +6,74 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = mysql.connector.connect(
-    host="localhost",
+    host="127.0.0.1",
     user="root",
     port=3306,
-    password="",  # Gantilah dengan password MySQL Anda
-    database="Veggie_db"
+    password="",
+    database="veggie_db"
 )
 
 cursor = db.cursor()
 
 
-@app.route('/api/data', methods=['GET', 'POST'])
-def handle_data():
-    if request.method == 'GET':
-        data_type = request.args.get('type')
-        if data_type == 'sayur':
-            cursor.execute("SELECT * FROM sayur")
-        elif data_type == 'pasar':
-            cursor.execute("SELECT * FROM pasar")
-        elif data_type == 'deteksi':
-            cursor.execute("SELECT * FROM deteksi")
-        else:
-            return jsonify({'error': 'Invalid data type'}), 400
-
-        result = cursor.fetchall()
-        data = []
-        for row in result:
-            if data_type == 'sayur':
-                data.append({
-                    'id_sayur': row[0],
-                    'nama_sayur': row[1],
-                    'gambar_sayur': row[2],
-                    'tanggal_input': row[3],
-                })
-            elif data_type == 'pasar':
-                data.append({
-                    'id_pasar': row[0],
-                    'nama_pasar': row[1],
-                })
-            elif data_type == 'deteksi':
-                data.append({
-                    'id_deteksi': row[0],
-                    'id_sayur': row[1],
-                    'id_pasar': row[2],
-                    'hasil_deteksi': row[3],
-                })
-
-        return jsonify(data)
-
-    elif request.method == 'POST':
-        try:
-            data_type = request.form.get('type')
-            if data_type == 'sayur':
-                if 'nama_sayur' in request.form and 'tanggal_input' in request.form and 'gambar_sayur' in request.files:
-                    # Pengolahan gambar
-                    uploaded_file = request.files['gambar_sayur']
-                    if uploaded_file and allowed_file(uploaded_file.filename):
-                        filename = secure_filename(uploaded_file.filename)
-                        filepath = os.path.join(
-                            app.config['UPLOAD_FOLDER'], filename)
-                        uploaded_file.save(filepath)
-                    else:
-                        return jsonify({'error': 'Invalid image file'}), 400
-
-                    # Pengolahan tanggal dan waktu
-                    tanggal_input = request.form.get('tanggal_input')
-                    try:
-                        tanggal_input = datetime.strptime(
-                            tanggal_input, '%Y-%m-%d %H:%M:%S')
-                    except ValueError:
-                        return jsonify({'error': 'Invalid date & time format'}), 400
-
-                    query = "INSERT INTO sayur (nama_sayur, gambar_sayur, tanggal_input) VALUES (%s, %s, %s)"
-                    values = (request.form.get('nama_sayur'),
-                              filepath, tanggal_input)
-                else:
-                    return jsonify({'error': 'Invalid form data'}), 400
-            elif data_type == 'pasar':
-                if 'nama_pasar' in request.form:
-                    query = "INSERT INTO pasar (nama_pasar) VALUES (%s)"
-                    values = (request.form.get('nama_pasar'),)
-                else:
-                    return jsonify({'error': 'Invalid form data'}), 400
-            elif data_type == 'deteksi':
-                if 'id_sayur' in request.form and 'id_pasar' in request.form and 'hasil_deteksi' in request.form:
-                    query = "INSERT INTO deteksi (id_sayur, id_pasar, hasil_deteksi) VALUES (%s, %s, %s)"
-                    values = (request.form.get('id_sayur'), request.form.get(
-                        'id_pasar'), request.form.get('hasil_deteksi'))
-                else:
-                    return jsonify({'error': 'Invalid form data'}), 400
-            else:
-                return jsonify({'error': 'Invalid form data type'}), 400
-
-            cursor.execute(query, values)
-            db.commit()
-            return jsonify({'message': f'Data {data_type} berhasil ditambahkan'}), 201
-        except Exception as e:
-            return jsonify({'error': 'Internal Server Error', 'details': str(e)}), 500
-
 # Fungsi untuk memeriksa ekstensi gambar yang diunggah
-
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/api/data', methods=['GET', 'POST'])
+def handle_data():
+    if request.method == 'GET':
+        # Implement your GET logic here to retrieve data from the database
+        # Use request.args to access query parameters
+
+        return jsonify({'message': 'This is a GET request'})
+
+    elif request.method == 'POST':
+        try:
+            nama_sayur = request.form.get('nama_sayur')
+            nama_pasar = request.form.get('nama_pasar')
+
+            # Look up the 'id_sayur' and 'id_pasar' based on provided names
+            cursor.execute("SELECT id_sayur FROM sayur WHERE nama_sayur = %s", (nama_sayur,))
+            result_sayur = cursor.fetchone()
+
+            cursor.execute("SELECT id_pasar FROM pasar WHERE nama_pasar = %s", (nama_pasar,))
+            result_pasar = cursor.fetchone()
+
+            if result_sayur is None:
+                return jsonify({'error': 'Nama sayur not found'}), 404
+
+            if result_pasar is None:
+                return jsonify({'error': 'Nama pasar not found'}), 404
+
+            id_sayur = result_sayur[0]
+            id_pasar = result_pasar[0]
+
+            gambar_sayur = request.files['gambar_sayur']
+            if gambar_sayur and allowed_file(gambar_sayur.filename):
+                filename = secure_filename(gambar_sayur.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                gambar_sayur.save(filepath)
+            else:
+                return jsonify({'error': 'Invalid image file'}), 400
+
+            hasil_deteksi = request.form.get('hasil_deteksi')
+
+            tanggal_input = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            query = "INSERT INTO deteksi (id_sayur, id_pasar, gambar_sayur, tanggal_input, hasil_deteksi) VALUES (%s, %s, %s, %s, %s)"
+            values = (id_sayur, id_pasar, filepath, tanggal_input, hasil_deteksi)
+
+            cursor.execute(query, values)
+            db.commit()
+            return jsonify({'message': 'Data deteksi berhasil ditambahkan'}), 200
+        except Exception as e:
+            return jsonify({'error': 'Internal Server Error', 'details': str(e)}), 500
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
